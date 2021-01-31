aaa7f8aba20f40a33111662bbbb95fff71a723e1
James Sarmiento <andrew@codeninja.co>
2021-01-31 22:29:03 +0800

    Fix yolo_data from data

--- scripts/prepare_yolo_data.sh
+++ scripts/prepare_yolo_data.sh
@@ -13,8 +13,8 @@ cp data/val/annotations/*.xml yolo_data/labels/val
 
 pip install pillow
 
-python utils/xml2yolo.py --classes $(pwd)/artifacts/damage_list.txt --anns $(pwd)/data/val/annotations
-python utils/xml2yolo.py --classes $(pwd)/artifacts/damage_list.txt --anns $(pwd)/data/train/annotations
+python utils/xml2yolo.py --classes $(pwd)/artifacts/damage_list.txt --anns $(pwd)/yolo_data/val/annotations
+python utils/xml2yolo.py --classes $(pwd)/artifacts/damage_list.txt --anns $(pwd)/yolo_data/train/annotations
 
-rm $(pwd)/data/val/annotations/*.xml
-rm $(pwd)/data/train/annotations/*.xml
+rm $(pwd)/yolo_data/val/annotations/*.xml
+rm $(pwd)/yolo_data/train/annotations/*.xml
