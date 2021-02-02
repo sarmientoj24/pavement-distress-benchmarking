57c2aaf00aa19b1f913a7c051fadb94c6b319340
James Sarmiento <andrew@codeninja.co>
2021-02-02 21:18:56 +0800

    Add download scripts for data processed by CLAHE

---
+++ scripts/download_rddc_full_cl.sh
@@ -0,0 +1,4 @@
+gdown https://drive.google.com/uc?id=1iO8uNUVOk9pZhaako2qnInkvnDWOf3_N
+unzip rddc2020_data_cl.zip
+mv rddc2020_data_cl data
+rm rddc2020_data_cl.zip
---
+++ scripts/download_rddc_sample_cl.sh
@@ -0,0 +1,4 @@
+gdown https://drive.google.com/uc?id=1--DSuBabiaeYuJrsJD3m1114rDoWpJJ7
+unzip rddc2020_sample_cl.zip
+mv rddc2020_sample_cl data
+rm rddc2020_sample_cl.zip
