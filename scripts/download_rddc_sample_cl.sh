a70bb29dfadb153741d8fe17b4a8ff346fcd33c6
James Sarmiento <andrew@codeninja.co>
2021-02-02 23:30:32 +0800

    Change link for sample cl rddc

--- scripts/download_rddc_sample_cl.sh
+++ scripts/download_rddc_sample_cl.sh
@@ -1,4 +1,4 @@
-gdown https://drive.google.com/uc?id=1--DSuBabiaeYuJrsJD3m1114rDoWpJJ7
+gdown https://drive.google.com/uc?id=1DeEeazrIQsbrrUJ_Teg6FhYtNqmYhm_O
 unzip rddc2020_sample_cl.zip
 mv rddc2020_sample_cl data
 rm rddc2020_sample_cl.zip
