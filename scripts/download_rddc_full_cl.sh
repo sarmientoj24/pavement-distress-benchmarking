65e877015ba1cc63afb251a3f76e903cac4dbb80
James Sarmiento <andrew@codeninja.co>
2021-02-03 00:15:07 +0800

    Change full cl rddc link

--- scripts/download_rddc_full_cl.sh
+++ scripts/download_rddc_full_cl.sh
@@ -1,4 +1,4 @@
-gdown https://drive.google.com/uc?id=1iO8uNUVOk9pZhaako2qnInkvnDWOf3_N
+gdown https://drive.google.com/uc?id=1-0ul5RLEsLOTZRrCskLr9MApI7liztK2
 unzip rddc2020_data_cl.zip
 mv rddc2020_data_cl data
 rm rddc2020_data_cl.zip
